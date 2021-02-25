833802c8c519a062562d178d01f33a733443fa46
TristanLee <tlee10@stuy.edu>
2021-02-25 00:34:21 -0500

    final commit

--- draw.py
+++ draw.py
@@ -1,4 +1,54 @@
 from display import *
 
 def draw_line( x0, y0, x1, y1, screen, color ):
-    pass
+    [(i,j),(k,l)]=sorted([(x0,y0),(x1,y1)])
+    a=l-j
+    b=i-k
+    x = i
+    y = j
+    if b==0:
+        while y<max(j,l):
+            plot(screen,color,x,y)
+            y+=1
+        return
+    m=-a/b
+    # Quadrant 1 and 5
+    if 0<=m<=1:
+        d=2*a+b
+        while x<=k:
+            plot(screen,color,x,y)
+            if d>0:
+                y+=1
+                d+=2*b
+            x+=1
+            d+=2*a
+    # Quadrant 2 and 6
+    elif 1<m:
+        d=a+2*b
+        while y<l:
+            plot(screen,color,x,y)
+            if d<0:
+                x+=1
+                d+=2*a
+            y+=1
+            d+=2*b
+    # Quadrant 3 and 7
+    elif m<-1:
+        d=a+2*b
+        while y>l:
+            plot(screen,color,x,y)
+            if d>0:
+                x+=1
+                d+=2*a
+            y-=1
+            d-=2*b
+    # Quadrant 4 and 8
+    else:
+        d = 2 * a + b
+        while x <= k:
+            plot(screen, color, x, y)
+            if d < 0:
+                y -= 1
+                d -= 2 * b
+            x += 1
+            d += 2 * a
--- main.py
+++ main.py
@@ -6,35 +6,77 @@ c = [ 0, 255, 0 ]
 
 #octants 1 and 5
 draw_line(0, 0, XRES-1, YRES-1, s, c)
-draw_line(0, 0, XRES-1, YRES / 2, s, c)
-draw_line(XRES-1, YRES-1, 0, YRES / 2, s, c)
+draw_line(0, 0, XRES-1, YRES // 2, s, c)
+draw_line(XRES-1, YRES-1, 0, YRES // 2, s, c)
 
 #octants 8 and 4
 c[BLUE] = 255
 draw_line(0, YRES-1, XRES-1, 0, s, c)
-draw_line(0, YRES-1, XRES-1, YRES/2, s, c)
-draw_line(XRES-1, 0, 0, YRES/2, s, c)
+draw_line(0, YRES-1, XRES-1, YRES//2, s, c)
+draw_line(XRES-1, 0, 0, YRES//2, s, c)
 
 #octants 2 and 6
 c[RED] = 255
 c[GREEN] = 0
 c[BLUE] = 0
-draw_line(0, 0, XRES/2, YRES-1, s, c)
-draw_line(XRES-1, YRES-1, XRES/2, 0, s, c)
+draw_line(0, 0, XRES//2, YRES-1, s, c)
+draw_line(XRES-1, YRES-1, XRES//2, 0, s, c)
 
 #octants 7 and 3
 c[BLUE] = 255
-draw_line(0, YRES-1, XRES/2, 0, s, c)
-draw_line(XRES-1, 0, XRES/2, YRES-1, s, c)
+draw_line(0, YRES-1, XRES//2, 0, s, c)
+draw_line(XRES-1, 0, XRES//2, YRES-1, s, c)
 
 #horizontal and vertical
 c[BLUE] = 0
 c[GREEN] = 255
-draw_line(0, YRES/2, XRES-1, YRES/2, s, c)
-draw_line(XRES/2, 0, XRES/2, YRES-1, s, c)
+draw_line(0, YRES//2, XRES-1, YRES//2, s, c)
+draw_line(XRES//2, 0, XRES//2, YRES-1, s, c)
 
 
 display(s)
 save_ppm(s, 'binary.ppm')
 save_ppm_ascii(s, 'ascii.ppm')
 save_extension(s, 'img.png')
+
+def my_own():
+    from math import cos,sin
+    DEFAULT_COLOR[0]=255
+    DEFAULT_COLOR[1]=255
+    DEFAULT_COLOR[2]=255
+    s=new_screen()
+    c=[0,0,0]
+
+    # x-axis
+    draw_line(0,YRES//2,XRES,YRES//2,s,c)
+    # y-axis
+    draw_line(XRES//2,0,XRES//2,YRES,s,c)
+
+    c=[220,0,0]
+    density=4.5
+
+    # derivative of xsin(x)
+    f_prime = lambda x: x*cos(x/density)/density+sin(x/density)
+
+    lasty=YRES//2
+    for x in range(XRES//2,XRES-1):
+        lin=f_prime(x-XRES//2)
+        draw_line(x,lasty,x+1,lasty+int(lin),s,c)
+        lasty+=int(lin)
+    lasty = YRES // 2
+    for x in range(XRES // 2, 0,-1):
+        lin = f_prime(x - XRES // 2)
+        draw_line(x, lasty, x - 1, lasty - int(lin), s, c)
+        lasty -= int(lin)
+
+    # squeeze
+    c=[0,100,255]
+    draw_line(0,0,XRES,YRES,s,c)
+    draw_line(0,YRES,XRES,0,s,c)
+
+    display(s)
+    save_ppm(s, 'binary.ppm')
+    save_ppm_ascii(s, 'ascii.ppm')
+    save_extension(s, 'img.png')
+
+my_own()
